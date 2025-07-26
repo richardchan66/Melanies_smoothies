684ca7d6c5a4858fbf6954a92fd19d3ccb655de7
Richard Chan <richardchan66@gmail.com>
2025-07-26 18:16:36 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+rch_on# Import python packages
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
@@ -36,7 +36,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
