6e3af282bbd5552c46e46bf9fcbb296f41fe2549
Richard Chan <richardchan66@gmail.com>
2025-07-26 17:06:34 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,6 @@ if time_to_insert:
     st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+
 st.text(smoothiefroot_response())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
